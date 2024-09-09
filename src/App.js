85114242554f8f92b0925b7d551e860d7db80646
kimdayeon37 <kdy37912@naver.com>
2024-09-09 15:00:50 +0900

    feat: router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Button from '@mui/material/Button';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/common/Navbar/Navbar";
+import MainPage from "./components/views/MainPage/MainPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import CalendarPage from "./components/views/CalendarPage/CalendarPage";
+import MyPage from "./components/views/MyPage/MyPage";
 
 function App() {
   return (
-    <Button variant="contained">
-      Hello World
-    </Button>  
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/calendar" element={<CalendarPage />} />
+      <Route path="/mypage" element={<MyPage />} />
+    </Routes>
+  </Router>
     );
 }
 
