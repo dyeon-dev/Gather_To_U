0dfc23e522563c5ffc5422365dc86f3df8151a2e
kimdayeon37 <kdy37912@naver.com>
2024-09-11 17:11:02 +0900

    setting: server 환경세팅

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const bodyParser = require("body-parser"); // req.body
+const cookieParser = require("cookie-parser");
+
+// url 파싱해서 데이터 가져올 수 있도록 함
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 타입으로 온 데이터도 분석해서 가져올 수 있도록 함
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+// MongoDB
+const mongoose = require("mongoose");
+mongoose // 몽구스를 이용해서 mongoDB에 연결
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
